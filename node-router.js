30fb4ad9c1bf1ebe89683a9c041ffbed72e24fde
silentgamelan <28412081+SilentGamelan@users.noreply.github.com>
2019-01-16 23:13:42 +0000

    Sets up basic web server and adds blank router file

---
+++ node-router.js
@@ -0,0 +1,5 @@
+// node-router.js
+// 
+// Recreates functionality of server.js using vanilla node rather than express
+// Extracts header information from HTTP request and returns a JSON object
+
---
+++ node-server.js
@@ -0,0 +1,19 @@
+// node-server.js
+//
+// Basic webserver created with vanilla node.js
+// Recreates functionality found in server.js without using express
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
