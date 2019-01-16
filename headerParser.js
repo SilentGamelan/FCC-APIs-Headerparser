efbf1aa07219b1bfc29eacf9cd22d1720ee1c2c4
silentgamelan <28412081+SilentGamelan@users.noreply.github.com>
2019-01-16 22:42:28 +0000

    Adds header parsing and returns JSON

--- headerParser.js
+++ headerParser.js
@@ -10,6 +10,16 @@ router.get("/api/hello", function (req, res) {
     res.json({greeting: 'hello API'});
   });
   
-
+router.get("/api/whoami", (req, res) => {
+    // use req.get() to access header of known name
+    // req.headers to access the array containing all headers
+    let parsedHeader = {
+        "ipaddress": req.ip,
+        "language": req.get("accept-language"),
+        "software": req.get("user-agent")
+    }
+    console.log(req.headers);
+    res.json(parsedHeader);
+});
 
 module.exports = router;
