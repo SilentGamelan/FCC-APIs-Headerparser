62e14a44d26d4dcbb2a20267a477c7f3dcfb3231
silentgamelan <28412081+SilentGamelan@users.noreply.github.com>
2019-01-17 00:41:57 +0000

    Adds header parsing and JSON response on /api/whoami route

--- node-server.js
+++ node-server.js
@@ -4,14 +4,59 @@
 // Recreates functionality found in server.js without using express
 
 const http = require('http');
+const URL = require('url'); 
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+/*
+// Status code 200 = standard HTTP success response (OK)
+// Content type = Message Body Information Header
+// res.end() = end HTTP response, can send optional text only message
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
+});
+*/
+
+const server = http.createServer((req, res) => {
+    // destructuring syntax -> { x, y } = obj 
+    const { headers, method, url } = req;
+    const OK = 200;
+
+    req.on('error', (err) => {
+        console.error(err);
+        res.statusCode = 400;
+        res.end('Error: Please contact the administrator');
+    });
+
+    res.on('error', (err) => {
+        console.log(err);
+    })
+
+    let path = URL.parse(url, true).pathname;
+    let ip = headers['x-forwarded-for'] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    
+      
+    if(method === 'GET') {
+
+        if(path === '/api/whoami') {            
+            res.writeHead(OK, { 'content-type': 'application/json'});
+            let parsedHeader = {
+                "ipaddress": ip,
+                "language": headers["accept-language"],
+                "software": headers["user-agent"]
+            }
+            res.write(JSON.stringify(parsedHeader));
+            
+        }
+    }
+        
 });
 
 server.listen(port, hostname, () => {
